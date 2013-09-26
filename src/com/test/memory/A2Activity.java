f9fcd29b91069b6508b547aebc30037ca01a04bc
Anurak Phoolsawat <anurak@palomarsystems.com>
2013-09-26 19:01:18 +0700

    experiment2 start close page

--- src/com/test/memory/A2Activity.java
+++ src/com/test/memory/A2Activity.java
@@ -14,7 +14,8 @@ public class A2Activity extends Activity {
 	}
 	
 	public void next(View view) {
-		Intent intent = new Intent(getApplicationContext(), A1Activity.class);
+		Intent intent = new Intent(getApplicationContext(), A3Activity.class);
 		startActivity(intent);
+		finish();
 	}
 }
